4c7e7a4fb425fbf52c8a577cc94d95f46f082ef6
msiric <mario.siric@mail.com>
2024-08-02 17:58:52 +0200

    fix: edit logo theme switch

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,43 @@
 import LogoIconSvg from '/public/images/brand/icon.svg'
-import LogoDefaultSvg from '/public/images/brand/logo-light.svg'
-import LogoSquaredSvg from '/public/images/brand/logo-squared-light.svg'
+import LogoDefaultSvgDark from '/public/images/brand/logo-dark.svg'
+import LogoDefaultSvgLight from '/public/images/brand/logo-light.svg'
+import LogoSquaredSvgDark from '/public/images/brand/logo-squared-dark.svg'
+import LogoSquaredSvgLight from '/public/images/brand/logo-squared-light.svg'
 
-export function Logo(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return <LogoDefaultSvg {...props} />
+type LogoProps = React.ComponentPropsWithoutRef<'svg'>
+
+const Logo = (props: LogoProps) => {
+  return (
+    <>
+      <LogoDefaultSvgDark
+        {...props}
+        className={`${props.className} dark:hidden`}
+      />
+      <LogoDefaultSvgLight
+        {...props}
+        className={`${props.className} hidden dark:block`}
+      />
+    </>
+  )
 }
 
-export function LogoIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+const LogoIcon = (props: LogoProps) => {
   return <LogoIconSvg {...props} />
 }
 
-export function LogoSquared(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return <LogoSquaredSvg {...props} />
+const LogoSquared = (props: LogoProps) => {
+  return (
+    <>
+      <LogoSquaredSvgDark
+        {...props}
+        className={`${props.className} dark:hidden`}
+      />
+      <LogoSquaredSvgLight
+        {...props}
+        className={`${props.className} hidden dark:block`}
+      />
+    </>
+  )
 }
+
+export { Logo, LogoIcon, LogoSquared }
